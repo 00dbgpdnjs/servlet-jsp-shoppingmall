fbcfbd05d370034e4e10020491eba723e8aa99ed
00dbgpdnjs <00dbgpdnjs@naver.com>
2024-12-01 12:51:30 +0900

    .

---
+++ src/main/java/com/nhnacademy/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.nhnacademy.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+// 명시적으로 사용할 필요x
+// Spring Boot는 자동 설정(autoconfiguration)을 통해 Spring Security를 기본적으로 활성화하고, 기본적인 보안 설정을 제공
+// ;@SpringBootApplication 어노테이션이 이미 **@EnableAutoConfiguration**을 포함
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf((auth) -> auth.disable());
+
+        http
+                .formLogin((auth) -> auth.disable());
+
+        //HTTP Basic 인증 방식 disable
+        http
+                .httpBasic((auth) -> auth.disable());
+
+        http
+                .oauth2Login(Customizer.withDefaults());
+
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated());
+
+        //세션 설정 : STATELESS
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+}
